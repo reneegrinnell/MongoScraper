2b84738080be4f71939e650f9c1be9c75b6a487e
Renée Grinnell <renee.grinnell@gmail.com>
2018-12-15 15:55:34 -0500

    scraping success!

--- models/Article.js
+++ models/Article.js
@@ -17,16 +17,16 @@ var ArticleSchema = new Schema({
     required: true
   },
   // 'image' is required and of type String
-  image: {
-      type: String,
-      required: true
-  },
+  // image: {
+  //     type: String,
+  //     required: true
+  // },
   // `note` is an object that stores a Note id
   // The ref property links the ObjectId to the Note model
   // This allows us to populate the Article with an associated Note
   comment: {
     type: Schema.Types.ObjectId,
-    ref: "Comment"
+    ref: "Note"
   }
 });
 
